f1204e6fc4dd4731c370ed74778f4365f0de0289
Seth <silver1row@cox.net>
2018-07-04 01:19:58 -0500

    Testing more...erase the time.
    Hello,
    
    Line 10 was useless but fun. I got rid of time.
    
    Seth

--- src/HoursAndPython.py
+++ src/HoursAndPython.py
@@ -1,13 +1,13 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
-from time import time
+import time
 
 PWM.start("P8_13", 50, 2000, 1)
 EnA = ("P8_08", 0.0)
 
 print("Enter Sun Rise: ")
 
-SunRise = bool(raw_input(time))
+SunRise = bool(raw_input())
 Hour = 6.15
 
 if Hour > 6.15:

4d5171cb8783f28fae131ee1b0ad364fac3191eb
Adam Powers <17037082+badfrogg@users.noreply.github.com>
2018-07-03 21:07:05 -0400

    Update TestPWM.js

--- src/TestPWM.js
+++ src/TestPWM.js
@@ -1,3 +1,3 @@
 var b = require('bonescript');
-b.analogWrite('P9_14', 0.7, 2000, printJSON);
+b.analogWrite('P9_14', 0.5, 22000, printJSON);
 function printJSON(x) { console.log(JSON.stringify(x)); }

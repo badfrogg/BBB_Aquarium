2bcad296bcfff27f7672e799c8034fe93b28700c
Adam Powers <17037082+badfrogg@users.noreply.github.com>
2018-07-04 00:05:48 -0400

    Update TestPWM.js
    This runs in a loop and increments the duty cycle from 0 to 100 and then back down to 0 repeating endlessly. The Frequency is hard set to 12kHz. This is just testing code for now and has no practical purpose.

--- src/TestPWM.js
+++ src/TestPWM.js
@@ -1,3 +1,37 @@
 var b = require('bonescript');
-b.analogWrite('P9_14', 0.5, 22000, printJSON);
-function printJSON(x) { console.log(JSON.stringify(x)); }
+
+// Setting the freq to 12kHz,  Duty is 0% to 100%
+var PWM_PIN = 'P9_14';
+var duty_min = 0;
+var duty_max = 100;
+var duty = 0;
+var freq = 12000;
+var increment = 10;
+
+// Set up the mode for the PWM_PIN pin
+b.pinMode(PWM_PIN, b.ANALOG_OUTPUT);
+updateDuty();
+console.log('Use keyboard Control-C to stop');
+
+function updateDuty() {
+	// This function calculates and adjusts the duty cycle based on a desired duty in range 0 to 100
+	var duty_cycle = (duty*0.01) + duty_min;
+	b.analogWrite(PWM_PIN, duty_cycle, freq, scheduleNextUpdate);
+	// Print out the values of duty_cycle and duty variables for debugging
+	console.log('duty cycle: ' + parseFloat(duty_cycle).toFixed(1) + ' duty: ' + duty + '%');
+}
+
+function scheduleNextUpdate() {
+	// adjust duty by increment and reverse if it exceeds range of 0..1
+	duty = duty + increment;
+	if(duty < 0) {
+		duty = 0;
+		increment = -increment;
+	}
+	else if(duty > 100) {
+		duty = 100;
+		increment = -increment;
+	}
+// call updateDuty after 500ms
+setTimeout(updateDuty, 500);
+}
